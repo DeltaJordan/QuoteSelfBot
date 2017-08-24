c337a43d4714cbd0726c4cd218c1a86c7c752049
John Strebeck <thegreatplanetmojang@gmail.com>
2017-08-24 17:23:20 -0500

    Add project files.

---
+++ QuoteSelfBot/Commands/Commands.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace QuoteSelfBot.Commands
+{
+    public class Commands : ModuleBase
+    {
+        private int quoteRetries;
+
+        [Command("quote")]
+        public async Task Quote(ulong messageId, int cache = 100)
+        {
+            this.quoteRetries++;
+
+            try
+            {
+                List<IReadOnlyCollection<IMessage>> messageCache = await this.Context.Channel.GetMessagesAsync(cache).ToList();
+
+                IMessage message = null;
+
+                foreach (IReadOnlyCollection<IMessage> readOnlyCollection in messageCache)
+                {
+                    if (readOnlyCollection.FirstOrDefault(e => e.Id == messageId) != null)
+                    {
+                        message = readOnlyCollection.FirstOrDefault(e => e.Id == messageId);
+                    }
+                }
+
+                if (message == null)
+                {
+                    if (this.quoteRetries == 10)
+                    {
+                        Console.WriteLine("Unable to get message");
+                    }
+
+                    await this.Quote(messageId, cache + 100);
+                    return;
+                }
+
+                string authorName;
+
+                if (message.Author is IGuildUser user)
+                {
+                    authorName = string.IsNullOrWhiteSpace(user.Nickname) ? user.Nickname : user.Username;
+                }
+                else
+                {
+                    authorName = message.Author.Username;
+                }
+
+                EmbedBuilder builder = new EmbedBuilder
+                {
+                    Description = message.Content,
+                    Footer = new EmbedFooterBuilder
+                    {
+                        Text = message.Timestamp.ToLocalTime().ToString()
+                    }
+                };
+
+                Console.WriteLine(authorName);
+
+                builder.WithAuthor(new EmbedAuthorBuilder
+                {
+                    IconUrl = message.Author.GetAvatarUrl(),
+                    Name = authorName
+                });
+
+                await this.Context.Message.DeleteAsync();
+                await this.ReplyAsync(string.Empty, false, builder.Build());
+
+                this.quoteRetries = 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ QuoteSelfBot/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QuoteSelfBot
+{
+    class Program
+    {
+        public static readonly string AppPath = Directory.GetParent(new Uri(Assembly.GetEntryAssembly().CodeBase).LocalPath).FullName;
+        public static DiscordSocketClient Client;
+        public CommandService Commands;
+        private IServiceProvider serviceProvider;
+        private string commandHeader;
+
+        private async Task MainAsync()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+                MessageCacheSize = 1000,
+                AlwaysDownloadUsers = true,
+                LargeThreshold = 200000
+            });
+
+            this.Commands = new CommandService(new CommandServiceConfig
+            {
+                DefaultRunMode = RunMode.Async,
+                CaseSensitiveCommands = false
+            });
+
+            this.serviceProvider = new ServiceCollection().BuildServiceProvider();
+
+            Client.Log += Log;
+            this.Commands.Log += Log;
+
+            await this.InstallCommands();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path.Combine(AppPath, "config.xml"));
+            XmlNodeList xmlNodeList = doc.SelectNodes("/Settings/Token");
+            if (xmlNodeList != null)
+            {
+                string token = xmlNodeList[0].InnerText;
+
+                try
+                {
+                    await Client.LoginAsync(TokenType.User, token);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Token invalid!\n\nException: " + exception.ToString());
+                    throw;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid config file!");
+                Environment.Exit(404);
+            }
+
+            xmlNodeList = doc.SelectNodes("/Settings/Seperator");
+            if (xmlNodeList != null)
+            {
+                this.commandHeader = xmlNodeList[0].InnerText;
+            }
+            else
+            {
+                Console.WriteLine("Invalid config file!");
+                Environment.Exit(404);
+            }
+
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommands()
+        {
+            // Hook the MessageReceived Event into our Command Handler
+            Client.MessageReceived += this.HandleCommand;
+
+            // Discover all of the commands in this assembly and load them.
+            await this.Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        public async Task HandleCommand(SocketMessage msg)
+        {
+            SocketUserMessage message = msg as SocketUserMessage;
+
+            if (message == null)
+            {
+                return;
+            }
+
+            if (message.Content.StartsWith(this.commandHeader) && message.Author.Id == Client.CurrentUser.Id)
+            {
+                int argPos = -1;
+                
+                if (message.HasStringPrefix(this.commandHeader, ref argPos))
+                {
+                    // Create a Command Context
+                    CommandContext context = new CommandContext(Client, message);
+
+                    // Execute the command. (result does not indicate a return value,
+                    // rather an object stating if the command executed succesfully)
+                    IResult result = await this.Commands.ExecuteAsync(context, argPos);
+                    if (!result.IsSuccess)
+                    {
+                        Console.Out.WriteLine(result.ErrorReason + $"\nCommand:\n{message.Content.Substring(this.commandHeader.Length)}");
+                    }
+                }
+            }
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.Message);
+            return Task.CompletedTask;
+        }
+
+        private static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+    }
+}
